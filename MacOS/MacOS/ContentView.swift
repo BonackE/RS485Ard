f8ae1417f73f98952295d227ee691426e8fd83aa
Jesse Kuehl <jkuehl0409@gmail.com>
2023-03-28 16:46:13 -0500

    work on reading available ports in MacOS

--- MacOS/MacOS/ContentView.swift
+++ MacOS/MacOS/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import WebKit
+import IOKit.serial
 
 struct ContentView: View {
     var body: some View {
@@ -14,20 +15,47 @@ struct ContentView: View {
     }
 }
 
-func getDevices() -> Data? {
-    if let path = Bundle.main.path(forResource: "devices", ofType: "json") {
-        do {
-            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-            print("successfully read data: \(data)")
-            
-            return data
-        } catch {
-            print("Error reading devices.json: \(error)")
-        }
-    } else {
-        print("Could not find device.json file.")
+//func getDevices() -> Data? {
+//    if let path = Bundle.main.path(forResource: "devices", ofType: "json") {
+//        do {
+//            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+//            print("successfully read data: \(data)")
+//
+//            return data
+//        } catch {
+//            print("Error reading devices.json: \(error)")
+//        }
+//    } else {
+//        print("Could not find device.json file.")
+//    }
+//    return nil
+//}
+
+func getSerialPorts() -> [String] {
+    let matchingDict = IOServiceMatching(kIOSerialBSDServiceValue)
+    var serialIterator: io_iterator_t = 0
+    var serialPortArray = [String]()
+    
+    let kernResult = IOServiceGetMatchingServices(kIOMainPortDefault, matchingDict, &serialIterator)
+    guard kernResult == KERN_SUCCESS else {
+        print("IOServiceGetMatchingServices error:", kernResult)
+        return serialPortArray
     }
-    return nil
+    
+    var portService: io_object_t = 1
+    repeat {
+        portService = IOIteratorNext(serialIterator)
+        if portService == 0 {
+            break
+        }
+        if let path = IORegistryEntryCreateCFProperty(portService, kIOCalloutDeviceKey as CFString, kCFAllocatorDefault, 0).takeRetainedValue() as? String {
+            serialPortArray.append(path)
+        }
+        IOObjectRelease(portService)
+    } while true
+    
+    IOObjectRelease(serialIterator)
+    return serialPortArray
 }
 
 struct HTMLView: NSViewRepresentable {
@@ -40,7 +68,8 @@ struct HTMLView: NSViewRepresentable {
         let userContentController = webView.configuration.userContentController
         userContentController.add(context.coordinator, name: "getJsonData")
         userContentController.add(context.coordinator, name: "error")
-        
+        userContentController.add(context.coordinator, name: "LoadPorts")
+        userContentController.add(context.coordinator, name: "Connect")
         
         return webView
     }
@@ -57,15 +86,39 @@ struct HTMLView: NSViewRepresentable {
     }
     
     class Coordinator: NSObject, WKScriptMessageHandler {
+//        let modbus = Modbus() // Store the modbus object as a member of the Coordinator
+//
+//        init(modbus: Modbus) {
+//            self.modbus = modbus
+//        }
+        
         func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-            if message.name == "getJsonData", let webView = message.webView {
-                let jsonData = getDevices()!
-                let str = String(data: jsonData, encoding: .utf8)!
-                let jsonStr = str.replacingOccurrences(of: "\n", with: "");                
-
-                print("jsonStr: \(jsonStr)")
-   
-                let script = "handleJsonData('\(jsonStr)');"
+//            if message.name == "getJsonData", let webView = message.webView {
+//                let jsonData = getDevices()!
+//                let str = String(data: jsonData, encoding: .utf8)!
+//                let jsonStr = str.replacingOccurrences(of: "\n", with: "");
+//
+//                print("jsonStr: \(jsonStr)")
+//
+//                let script = "handleJsonData('\(jsonStr)');"
+//                webView.evaluateJavaScript(script) { result, error in
+//                    if let error = error {
+//                        print("Error: \(error.localizedDescription)")
+//                    } else {
+//                        print("Result: \(result ?? "")")
+//                    }
+//                }
+//            }
+            if message.name == "error" {
+                let error = (message.body as? [String: Any])?["message"] as? String ?? "unknown"
+                print("JavaScript error: \(error)")
+            }
+            if message.name == "LoadPorts", let webView = message.webView {
+    
+                let ports = getSerialPorts()
+                print("ports found: \(ports)")
+                                
+                let script = "handlePortsData(\(ports));"
                 webView.evaluateJavaScript(script) { result, error in
                     if let error = error {
                         print("Error: \(error.localizedDescription)")
@@ -73,10 +126,10 @@ struct HTMLView: NSViewRepresentable {
                         print("Result: \(result ?? "")")
                     }
                 }
+                
             }
-            if message.name == "error" {
-                let error = (message.body as? [String: Any])?["message"] as? String ?? "unknown"
-                print("JavaScript error: \(error)")
+            if message.name == "Connect" {
+                
             }
         }
     }
--- MacOS/MacOS/modbus_Bridge.h
+++ MacOS/MacOS/modbus_Bridge.h
@@ -1,3 +1,5 @@
+
+
 //
 //  modbus-Bridge.h
 //  MacOS
